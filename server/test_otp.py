9d21cd72110d8b833b3ce670b9a2e7a95695dea8
akshay-sri123 <akshay@one2n.in>
2020-06-24 17:33:26 +0530

    Inital commit

--- server/test_otp.py
+++ server/test_otp.py
@@ -3,11 +3,10 @@ from otp import generate_otp, hash_function
 
 
 class Test(TestCase):
-  def test_generate_secret(self):
-    self.assertEqual("27597339",
-                     generate_otp(otp_length=3, otp_expiry=30,
-                                  otp_secret="hello", coefficient="None"))
+    def test_generate_otp(self):
+        self.assertEqual("1798492015", generate_otp(otp_expiry=30, otp_secret="hello", coefficient=1234))
+        self.assertEqual("1628551077", generate_otp(otp_expiry=30, otp_secret="hello", coefficient=0))
 
-  def test_hash_function(self):
-    self.assertEquals("364039319", hash_function("hello", "1234"))
-    self.assertTrue(hash_function("hello", 1234), msg="parameters should be str")
+    def test_hash_function(self):
+        self.assertEqual("364039319", hash_function("hello", "1234"))
+        self.assertTrue(hash_function("hello", 1234), msg="parameters should be str")
